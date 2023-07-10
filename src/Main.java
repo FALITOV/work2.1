20a6b26749d0b0b74b957a4786e6a3bda7c65f5c
Egorius188 <postulgin98@mail.ru>
2023-07-10 13:24:56 +0300

    work

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int ticket = 15000;
+        int km = 20;
+        System.out.println( "Бонус за покупку: " + ticket/km);
+
+    }
+}
